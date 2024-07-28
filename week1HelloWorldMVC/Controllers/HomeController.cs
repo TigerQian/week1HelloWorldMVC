0095aff9d5820fe701ee0707228fc98f0991a59a
TigerQian <1926508047@qq.com>
2024-07-28 20:54:31 +0800

    Controller methods and views
    Controller methods and views

--- week1HelloWorldMVC/Controllers/HomeController.cs
+++ week1HelloWorldMVC/Controllers/HomeController.cs
@@ -23,6 +23,21 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+    public IActionResult Services()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
